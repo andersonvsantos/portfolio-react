3c909d3a4d34db4556473facdb07af380b8d8b7c
Anderson V Santos <andersonvs.ju@gmail.com>
2024-11-03 21:54:39 -0300

    bug

---
+++ src/components/Project/index.tsx
@@ -0,0 +1,11 @@
+import Paragraf from '../Paragraf'
+import Title from '../Title'
+
+const Project = () => (
+  <div>
+    <Title>Projeto lista de tarefas</Title>
+    <Paragraf category="secondary">Teste</Paragraf>
+  </div>
+)
+
+export default Project
--- src/containers/Projects/index.tsx
+++ src/containers/Projects/index.tsx
@@ -1,8 +1,14 @@
+import Project from '../../components/Project'
 import Title from '../../components/Title'
 
 const Projects = () => (
   <section>
     <Title fontSize={16}>Projetos</Title>
+    <ul>
+      <li>
+        <Project />
+      </li>
+    </ul>
   </section>
 )
 
